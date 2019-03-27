825274679f01746d90e78e45b568f1c74a095ab7
tyler <tylercedge@gmail.com>
2019-03-27 16:50:02 -0400

    concert-this working cannot figure out how to iterate it over all objects possible though

--- liri.js
+++ liri.js
@@ -1,31 +1,64 @@
 var axios = require("axios");
 
+var moment = require('moment');
+
 // require("dotenv").config();
 
 // var keys = require("./keys.js");
 
 // var spotify = new Spotify(keys.spotify);
 
-var omdbAction = process.argv[2];
-var input = process.argv[3];
+var action = process.argv[2];
+var input = process.argv[3] || 'Mr. Nobody';
+
+function helpMe() {
+    console.log('-----------------------------');
+    console.log('Instructions: ');
+    console.log('-----------------------------');
+}
 
 function runAxiosOmdb() {
     // console.log('hi')
     axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
-    .then(function(response) {
-        console.log(response.data.Title, 
-                    response.data.Year, 
-                    response.data.imdbRating, 
-                    response.data.Country, 
-                    response.data.Language, 
-                    response.data.Plot, 
-                    response.data.Actors);
-    })
+        .then(function (response) {
+            console.log(response.data.Title,
+                response.data.Year,
+                response.data.imdbRating,
+                response.data.Country,
+                response.data.Language,
+                response.data.Plot,
+                response.data.Actors);
+        })
 }
 
-if (omdbAction === 'movie-this') {
-    // console.log(omdbAction);
+if (action === 'movie-this') {
+    // console.log(action);
     runAxiosOmdb();
+} else if (process.argv[2] === 'help') {
+    helpMe();
+} else if (input === 'Mr. Nobody') {
+    runAxiosOmdb();
+} else if (action === 'concert-this') {
+    bandInTown();
 }
+
+function bandInTown() {
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < 10; i++) {
+                var dateTest = response.data[i].venue.datatime
+                var converted = moment(dateTest).format('MMMM Do YYYY, h:mm:ss a')
+
+                console.log(response.data[i].venue.name,
+                    response.data[i].venue.city + ', ' + response.data[i].venue.country, converted);
+
+                
+                
+            }
+
+        })
+}
+
+
 
 

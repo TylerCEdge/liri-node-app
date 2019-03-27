58c59b6c13c1938dc20d2d2440d77a0dba644f79
tyler <tylercedge@gmail.com>
2019-03-27 15:38:23 -0400

    console log movie data working

--- liri.js
+++ liri.js
@@ -1,3 +1,31 @@
-require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+// require("dotenv").config();
+
+// var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
+
+var omdbAction = process.argv[2];
+var input = process.argv[3];
+
+function runAxiosOmdb() {
+    // console.log('hi')
+    axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+        console.log(response.data.Title, 
+                    response.data.Year, 
+                    response.data.imdbRating, 
+                    response.data.Country, 
+                    response.data.Language, 
+                    response.data.Plot, 
+                    response.data.Actors);
+    })
+}
+
+if (omdbAction === 'movie-this') {
+    // console.log(omdbAction);
+    runAxiosOmdb();
+}
+
+
